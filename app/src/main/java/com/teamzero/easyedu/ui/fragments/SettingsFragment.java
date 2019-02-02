ab8ce909041fee01cba180f74ce335893165954c
Tirth Patel <pateltirth454@gmail.com>
2019-02-02 21:24:39 +0530

    Added Empty Settings Fragment

---
+++ app/src/main/java/com/teamzero/easyedu/ui/fragments/SettingsFragment.java
@@ -0,0 +1,29 @@
+package com.teamzero.easyedu.ui.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.teamzero.easyedu.R;
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+}
